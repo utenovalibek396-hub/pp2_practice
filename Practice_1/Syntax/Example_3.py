5117bc9651a18b475fdc14dea056ac808274f3c5
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-01-28 10:39:53 +0500

    Create Example_3.py

---
+++ Practice_1/Syntax/Example_3.py
@@ -0,0 +1,10 @@
+if 10 < 15
+    print("Yes")
+else
+    print("Yes") # incorrect code. Because: A colon indicates the start of a condition.
+
+if 10 < 15:
+    print("Yes")
+else:
+    print("Yes") #correct code
+
