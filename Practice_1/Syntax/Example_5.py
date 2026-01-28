994aefa65f8c7294a05c7e22580a50a83d54b672
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-01-28 10:45:33 +0500

    Create Example_5.py

---
+++ Practice_1/Syntax/Example_5.py
@@ -0,0 +1,6 @@
+if x = 10:
+    print(x)
+#Syntax Error. Because:The comparison operator is used differently during a condition.For example: ==,!=,>=,<=,>,<
+
+if x == 10:
+    print(x) #correct code
