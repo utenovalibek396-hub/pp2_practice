980d888f645fed15f3bb099e55c594f3f79ad3b7
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-02-05 20:33:14 +0500

    Update

--- Practice_2/boolean/boolean_intro.py
+++ Practice_2/boolean/boolean_intro.py
@@ -2,3 +2,8 @@ a = True
 b = False
 print("Is it raining?",a)
 print("Do I have an umbrella?",b)
+x = 10
+y = 5
+print("Is x greater than y?", x > y)
+# True = 1 
+# False = 0
--- Practice_2/boolean/boolean_operators.py
+++ Practice_2/boolean/boolean_operators.py
@@ -1,8 +1,10 @@
-x = True
-y = False
-print(x and y)
-print(x or y)
-print(not x)
-print(not y)
-
-
+n = int(input("Enter your numnber: "))
+if n > 10 and n % 2 == 0:
+    print("Yes")
+if n > 10 or n % 2 == 0:
+    print("Ok")
+if not(n > 10):
+    print("...")
+# and - requires two conditions to be met
+# or - if one of the two conditions is met
+# not - negates
