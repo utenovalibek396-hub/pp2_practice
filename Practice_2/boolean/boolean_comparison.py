2b1a3c2943c59a61f97e7774567cf1da32b8d04d
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-02-05 20:10:25 +0500

    Update

--- Practice_2/boolean/boolean_comparison.py
+++ Practice_2/boolean/boolean_comparison.py
@@ -1,3 +1,8 @@
-a = 10
-b = 10
-print(a + b)
+x = 10
+y = 5
+
+print(x > y)        # True
+print(x == y)       # False
+print(x != y)       # True
+print((x > 0) and (y < 10))  # True
+print(not (x < y))  # True
