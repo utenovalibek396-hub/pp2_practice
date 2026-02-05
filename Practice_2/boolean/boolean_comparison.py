c6ef10768e77eab4085f77077627fa49c10a8ef1
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-02-05 20:16:50 +0500

    Update

--- Practice_2/boolean/boolean_comparison.py
+++ Practice_2/boolean/boolean_comparison.py
@@ -6,3 +6,7 @@ print(x == y)       # False
 print(x != y)       # True
 print((x > 0) and (y < 10))  # True
 print(not (x < y))  # True
+
+# Equality operators: ==(Equal to) , !=(Not equal to)
+# Relational operators: 
+# >(Greater than), <(less than), >=(Greater than or equal to), <=(Less than or equal to)
