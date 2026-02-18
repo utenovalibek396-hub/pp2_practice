1df3a7ada99a8a7593e2ea767150d7f64e492eef
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-02-18 10:01:00 +0500

    Delete quiz_1 directory

--- quiz_1/task1.py
+++
@@ -1,5 +0,0 @@
-def summ(x,y):
-    return x+y
-x = int(input())
-y = int(input())
-print(summ(x,y))
--- quiz_1/task5.py
+++
@@ -1,7 +0,0 @@
-list = list(map(int,input().split()))
-def summ(list):
-    summa = 0
-    for i in range(len(list)):
-        summa += list[i]
-    return summa
-print(summ(list))
