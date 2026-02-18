f45f03b5fa58d2b67e97792114e4b42fb91aa078
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-02-18 10:04:36 +0500

    Update

--- quiz_1/task2.py
+++
@@ -1,4 +0,0 @@
-def square(x):
-    return x**2
-x = int(input())
-print(square(x))
--- quiz_1/task7.py
+++
@@ -1,9 +0,0 @@
-class Greet:
-    def __init__(self,person):
-        self.person = person
-    def greet(self):
-        return self.person
-z = input().split()
-y = z[0] + " "+ z[1]
-x = Greet(y)
-print(x.greet())
