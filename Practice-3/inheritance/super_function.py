c91b03e3e35fb773c3b8dda831684ea559df34ad
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-02-20 13:29:37 +0500

    Update super_function.py

--- Practice-3/inheritance/super_function.py
+++ Practice-3/inheritance/super_function.py
@@ -6,3 +6,8 @@ class Dog(Animal):
         super().__init__(name)
 d = Dog("Aktos")
 print(d.name)
+#It avoids repeating code from the parent class.
+
+#It ensures that the parent class is properly initialized.
+
+#It works in single and multiple inheritance situations.
