8df1c7a44424a509f0a6588d9b5ebb0a872302c9
utenovalibek396-hub <utenovalibek396@gmail.com>
2026-03-20 09:56:13 +0500

    update

--- Practice-6/builtin_functions/enumerate_zip_examples.py
+++ Practice-6/builtin_functions/enumerate_zip_examples.py
@@ -3,6 +3,7 @@ from functools import reduce #reduce is function, functools is module
 nums = [1, 2, 3, 4]
 total = reduce(lambda x, y: x + y, nums) #lambda is anonymous function
 print(total)
+#reduce() is used to apply a function of two arguments cumulatively to the items of a sequence.
 #2
 names = ["Ali", "Aruzhan", "Dana"]
 for i, name in enumerate(names): #enumerate returns index and value of the list
